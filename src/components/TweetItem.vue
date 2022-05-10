7b38cd350c145c542057d9f1b770b06c57f89c00
Tich <tkausiyo@gmail.com>
2022-05-10 13:35:08 +0200

    form elements

--- src/components/TweetItem.vue
+++ src/components/TweetItem.vue
@@ -6,7 +6,7 @@
 
 <script>
 export default {
-  name: 'App',
+  name: 'TweetItem',
     props: {
         username: {
             type: String,
--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -10,15 +10,38 @@
             :tweet="tweet"
             @favourite="toggleFavourite" />
     </div>
+    <form class="tweet-form" @submit.prevent="createNewTweet">
+        <label for="newTweet"><strong>New Tweet</strong></label>
+        <textarea id="newTweet" rows="4" v-model="newTweet.content"></textarea>
+
+        <div>
+            <label for="newTweetType"><strong>Type: </strong></label>
+            <select id="newTweetType" v-model="newTweet.type">
+                <option
+                    :value="option.value"
+                    v-for="(option, index) in tweetTypes"
+                    :key="index" >
+                    {{ option.name }}
+                </option>
+            </select>
+        </div>
+        <button>Tweet!</button>
+    </form>
   </div>
 </template>
 
 <script>
 import TweetItem from "./TweetItem.vue"
+
 export default {
-    name: "App",
+    name: "UserProfile",
     data() {
         return {
+            newTweet: { type: 'instant', content: '' },
+            tweetTypes: [
+                { value: 'draft', name: 'Draft' },
+                { value: 'instant', name: 'Instant' }
+            ],
             followers: 0,
             user: {
                 id: 1,
@@ -45,6 +68,17 @@ export default {
         },
         toggleFavourite(id) {
             console.log('id', id)
+        },
+        createNewTweet() {
+            if(this.newTweet.content){
+                console.log('tweet type', this.newTweet.type)
+                console.log('tweet content', this.newTweet.content)
+                this.user.tweets.unshift({
+                    id: this.newTweet.content.length,
+                    content: this.newTweet.content
+                    }
+                )
+            }
         }
     },
     mounted() {
@@ -65,5 +99,10 @@ export default {
 background: rebeccapurple;
 color: white;
 border-radius: 5px;
+}
+
+.tweet-form {
+    display: flex;
+    flex-direction: column;
 }
 </style>
